e9df834b84ebf0034fb065b1f36b0313106a9aa6
Soumik <dasguptaarjo@gmail.com>
2018-02-23 19:50:23 +0530

    Push file(s)

---
+++ formact.php
@@ -0,0 +1,86 @@
+<?php
+// define variables and set to empty values
+$name = $email = $phone= $roll = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+	if (empty($_POST["name"])) 
+	{
+    	$nameErr = "Name is required";
+  	} 
+  	else 
+  	{
+    	$name = test_input($_POST["name"]);
+    	// check if name only contains letters and whitespace
+    	if (!preg_match("/^[a-zA-Z ]*$/",$name)) 
+    	{
+      		$nameErr = "Only letters and white space allowed"; 
+      	}
+    }
+
+	if (empty($_POST["email"])) 
+	{
+    	$emailErr = "Email is required";
+  	} 
+  	else 
+  	{
+    	$email = test_input($_POST["email"]);
+    	// check if e-mail address is well-formed
+    	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+    	{
+      	$emailErr = "Invalid email format"; 
+    	}
+  	}
+
+if (empty($_POST["Phone"])) 
+	{
+    	$phoneErr = "Phone is required";
+  	} 
+  	else 
+  	{
+    	$phone = test_input($_POST["phone"]);
+    }
+
+if (empty($_POST["roll"])) 
+	{
+    	$rollErr = "Roll Number required";
+  	} 
+  	else 
+  	{
+    	$roll = test_input($_POST["roll"]);
+    }
+
+
+}
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+include 'db_connection.php';
+ 
+$conn = OpenCon();
+ 
+echo "Connected Successfully";
+ 
+// Create connection
+$conn = OpenCon();
+
+$sql = "INSERT INTO `TestRun` (NAME, EMAIL, ROLL , PHONE) VALUES ($name, $email, $roll, $phone)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+CloseCo($conn);
+
+
+
+?>
